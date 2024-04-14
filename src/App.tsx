db79a7933c6cc3bb1100b3cc2a0e4e7d149a4a8e
Sawyer Bivens <sawyer.codes.stuff@gmail.com>
2024-04-14 10:38:52 -0500

    Start making site responsive + update favicon/title

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import { Nav } from "./Nav"
 import ChevronRight from "/chevron-right.svg"
 import Star from "/star.svg"
-import TestimonyPFP from "/testimony-pfp.jpg"
+// import TestimonyPFP from "/testimony-pfp.jpg"
 
 function App() {
   return (
     <main className="bg-blue-800 min-h-screen">
       <Nav />
-      <div className="flex flex-row p-32 gap-32">
-        <div className="flex-1 flex flex-col gap-12">
-          <h1 className="text-white font-bold text-6xl">Let&apos;s improve your ACT scores, together.</h1>
-          <h2 className="text-white font-semibold text-lg">Work with a current high-school student who has achieved a 35 ACT and a perfect PSAT to improve your ACT scores right here in North Little Rock, Arkansas.</h2>
-          <div className="bg-blue-900 p-8 flex flex-col gap-4 rounded-xl">
-            <p className="text-white font-semibold text">&quot;Jonah and Sawyer from Pulliam-Bivens Tutoring are 100% worth it for anyone trying to get their ACT scores higher! They helped me improve my ACT score by double digits.&quot;</p>
+      <div className="flex flex-col 2xl:flex-row py-16 px-8 sm:p-16 xl:px-32 2xl:p-32 gap-24 2xl:gap-32">
+        <div className="flex-1 flex flex-col gap-6 2xl:gap-12">
+          <h1 className="text-white font-bold text-2xl lg:text-5xl xl:text-6xl">Let&apos;s improve your ACT scores, together.</h1>
+          <h2 className="text-white font-semibold text-md lg:text-lg">Work with a current high-school student who has achieved a 35 ACT and a perfect PSAT to improve your ACT scores right here in North Little Rock, Arkansas.</h2>
+          <div className="bg-blue-900 p-6 sm:p-8 flex flex-col gap-4 rounded-xl">
+            <p className="text-white font-semibold text italic text-xs lg:text-base">&quot;Jonah Pulliam and Sawyer Bivens have been some of my most dedicated and hard-working students. I am sure that they can help improve your ACT score.&quot;</p>
             <div className="flex flex-row gap-4 items-center">
-              <img src={TestimonyPFP} alt="&gt;" className="rounded-full w-12 h-12"></img>
-              <div className="flex flex-row gap-2 items-baseline">
-                <p className="text-white font-bold text-lg">Jane Doe</p>
-                <p className="text-blue-200 font-semibold text-md">Class of 2026</p>
+              {/* <img src={TestimonyPFP} alt="&gt;" className="rounded-full w-12 h-12"></img> */}
+              <div className="flex flex-row gap-2 items-center">
+                <p className="text-white font-bold text-xs md:text-md">Bruce Maddox</p>
+                <p className="text-blue-200 font-semibold text-xs">AP Calculus Teacher</p>
               </div>
-              <div className="flex flex-row gap-1 text-yellow-500 font-bold items-center text-sm">
+              <div className="hidden sm:flex flex-row gap-1 text-yellow-500 font-bold items-center text-sm">
                 <img src={Star} alt="&gt;" className="h-4"></img>
                 <img src={Star} alt="&gt;" className="h-4"></img>
                 <img src={Star} alt="&gt;" className="h-4"></img>
@@ -30,14 +30,14 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="flex-1 flex flex-col gap-4">
+        <div className="flex-1 flex flex-col gap-8 sm:gap-4">
           {[1,2,3,4,5].map((key) => 
-            <div className="bg-white w-full rounded-lg flex flex-row" key={key}>
+            <div className="bg-white w-full rounded-lg flex flex-col sm:flex-row" key={key}>
               <div className="pl-8 py-4">
-                <p className="font-bold">ACT Tutoring - One on One</p>
-                <p className="font-semibold text-sm">5-7pm, April 10th 2024 @ 2801 Orange St NLR</p>
+                <p className="font-bold text-sm sm:text-base">ACT Tutoring - One on One</p>
+                <p className="font-semibold text-xs sm:text-sm">5-7pm, April 10th 2024 @ 2801 Orange St NLR</p>
               </div>
-              <button className="ml-auto px-8 py-4 bg-red-600 font-semibold text-white flex flex-row items-center gap-4 rounded-r-lg hover:gap-5 duration-100">
+              <button className="sm:ml-auto justify-center sm:justify-normal text-sm md:text-base px-4 md:px-8 py-3 sm:py-4 bg-red-600 font-semibold text-white flex flex-row items-center gap-2 md:gap-4 rounded-b-lg sm:rounded-bl-none sm:rounded-r-lg hover:gap-5 duration-100 shadow-lg sm:shadow-none">
                 <span>Book This Session</span>
                 <img src={ChevronRight} alt="&gt;" height={16}></img>
               </button>
--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,18 +1,36 @@
 import textmark from "/textmark.png"
 import ChevronRight from "/chevron-right.svg"
+import Menu from "/menu.svg"
+import Close from "/x.svg"
+import { useState } from "react"
 
 export function Nav() {
-    return <>
-        <div className="px-32 py-4 bg-white flex flex-row items-center">
-            <img src={textmark} alt="Pulliam-Bivens Tutoring" height={40} />
-            <div className="flex flex-row gap-16 ml-auto items-center">
-                <a className="text-black font-semibold cursor-pointer hover:underline">About Us</a>
+    let [navIsOpen, setNavIsOpen] = useState(false);
+    return <div className={`bg-white flex flex-col ${navIsOpen && "gap-4 shadow-lg"} md:shadow-none md:gap-0 px-16 xl:px-32 py-4`}>
+        <div className="flex flex-row items-center">
+            <img src={textmark} alt="Pulliam-Bivens Tutoring" className="h-8" />
+            <div className="hidden md:flex flex-row gap-6 md:gap-8 lg:gap-12 xl:gap-16 ml-auto items-center">
+                <a className="text-black font-semibold cursor-pointer hover:underline">Meet the Tutors</a>
                 <a className="text-black font-semibold cursor-pointer hover:underline">Testimonies</a>
                 <button className="bg-red-600 font-semibold text-white px-4 py-2 flex flex-row items-center gap-4 rounded-lg hover:gap-5 hover:-ml-1 duration-100">
                     <span>Book Us</span>
                     <img src={ChevronRight} alt="&gt;" height={16}></img>
                 </button>
             </div>
+            <div className="flex md:hidden ml-auto text-red-600 font-extrabold">
+                {navIsOpen 
+                    ? <img src={Close} alt="Close Menu" height={16} onClick={() => setNavIsOpen(false)}></img>
+                    : <img src={Menu} alt="Open Menu" height={16} onClick={() => setNavIsOpen(true)}></img>
+                }
+            </div>
         </div>
-    </>
+        {navIsOpen && <div className="md:hidden flex flex-col gap-2">
+            <a className="text-black font-semibold cursor-pointer hover:underline block bg-gray-50 px-3 py-2 rounded-md">Meet the Tutors</a>
+            <a className="text-black font-semibold cursor-pointer hover:underline block bg-gray-50 px-3 py-2 rounded-md">Testimonies</a>
+            <button className="bg-red-600 text-white font-semibold flex flex-row gap-2 py-2 px-3 justify-center items-center rounded-md">
+                <span>Book Us</span>
+                <img src={ChevronRight} alt="&gt;" height={16}></img>
+            </button>
+        </div>}
+    </div>
 }
