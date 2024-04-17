250df3843af3a4eaaf014bf03a32600bb6be2be8
Sawyer Bivens <sawyerbivens06@gmail.com>
2024-04-17 13:04:21 -0500

    Add cc to contact mailto: links for emails

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -14,11 +14,11 @@ export function Footer() {
             </div>
             <div className="flex flex-col gap-2 md:gap-1 flex-1">
                 <h2 className="font-semibold">Contact Us</h2>
-                <a className="text-sm bg-black bg-opacity-10 p-2 px-4 rounded-lg md:rounded-none md:p-0 md:bg-transparent md:text-blue-100 cursor-pointer hover:underline flex flex-row gap-1 items-center md:gap-0" href="mailto:jbpulliam06@gmail.com">
+                <a className="text-sm bg-black bg-opacity-10 p-2 px-4 rounded-lg md:rounded-none md:p-0 md:bg-transparent md:text-blue-100 cursor-pointer hover:underline flex flex-row gap-1 items-center md:gap-0" href="mailto:jbpulliam06@gmail.com?cc=sawyerbivens06@gmail.com">
                     <img src={Mail} className="h-3 md:hidden" />
                     <span>jbpulliam06@gmail.com</span>
                 </a>
-                <a className="text-sm bg-black bg-opacity-10 p-2 px-4 rounded-lg md:rounded-none md:p-0 md:bg-transparent md:text-blue-100 cursor-pointer hover:underline flex flex-row gap-1 items-center md:gap-0" href="mailto:sawyerbivens06@gmail.com">
+                <a className="text-sm bg-black bg-opacity-10 p-2 px-4 rounded-lg md:rounded-none md:p-0 md:bg-transparent md:text-blue-100 cursor-pointer hover:underline flex flex-row gap-1 items-center md:gap-0" href="mailto:sawyerbivens06@gmail.com?cc=jbpulliam06@gmail.com">
                     <img src={Mail} className="h-3 md:hidden" />
                     <span>sawyerbivens06@gmail.com</span>
                 </a>
